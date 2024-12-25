3a869a73ea71cef915bbbbfc72ec5de83a1514a1
adhirk09 <adithyakrishna866@gmail.com>
2024-12-25 19:27:46 +0530

    V2

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote as url_quote
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
